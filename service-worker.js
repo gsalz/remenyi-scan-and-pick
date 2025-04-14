fe97486484ae0b090210ac50e6762349788f6dca
Olivér Vető <vetooliver@icloud.com>
2025-04-14 11:39:54 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,25 @@
-// üres service worker
+
+const CACHE_NAME = 'scan-pick-v1';
+const urlsToCache = [
+  '/',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/manifest.json',
+  '/firebase.js'
+];
+
+// Telepítés
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Kérés elfogása
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
