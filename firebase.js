294925d1fbb4419489ce8273956f01de3e06c3ca
Olivér Vető <vetooliver@icloud.com>
2025-04-14 10:50:44 +0200

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,13 +1,1 @@
-
-// Firebase inicializálás
-const firebaseConfig = {
-  apiKey: "AIzaSyBqXYdVxxRfVUrwDsI2VlHQvpdhqr-oe9g",
-  authDomain: "remenyikeresztur.firebaseapp.com",
-  projectId: "remenyikeresztur",
-  storageBucket: "remenyikeresztur.firebasestorage.app",
-  messagingSenderId: "981331750744",
-  appId: "1:981331750744:web:d93439261f6c48a6c4bf2d",
-  measurementId: "G-CSQP9CS2QD"
-};
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+// dummy firebase config
